1783a7b93a77e45b839cc3130b879a45c8a76be8
Your Name <stoner.michann@gmail.com>
2021-04-23 15:05:19 -0600

    Create test files and test data files

---
+++ test/test-data/traveler-test-data.js
@@ -0,0 +1,34 @@
+const allTravelersData = [
+  {
+    "id": 1,
+    "name": "Ham Leadbeater",
+    "travelerType": "relaxer"
+    },
+    {
+    "id": 2,
+    "name": "Rachael Vaughten",
+    "travelerType": "thrill-seeker"
+    },
+    {
+    "id": 3,
+    "name": "Sibby Dawidowitsch",
+    "travelerType": "shopper"
+    },
+    {
+    "id": 4,
+    "name": "Leila Thebeaud",
+    "travelerType": "photographer"
+    },
+    {
+    "id": 5,
+    "name": "Tiffy Grout",
+    "travelerType": "thrill-seeker"
+    },
+    {
+    "id": 6,
+    "name": "Laverna Flawith",
+    "travelerType": "shopper"
+    }
+];
+
+export default allTravelersData;
