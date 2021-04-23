457ce3350b815fff3047a75a1a5c4bc2ad9ccc0a
Your Name <stoner.michann@gmail.com>
2021-04-23 14:23:32 -0600

    Store fetched data in an array to use later

--- src/api-calls.js
+++ src/api-calls.js
@@ -4,29 +4,32 @@ let apiCalls = {
   getAllTravelers() {
     const fetchAllTravelers = fetch('http://localhost:3001/api/v1/travelers')
       .then(response => response.json())
-      .then(data => data)
+      .then(data => data.travelers)
     return fetchAllTravelers;
   },
 
   getAllTrips() {
     const fetchAllTrips = fetch('http://localhost:3001/api/v1/trips')
       .then(response => response.json())
-      .then(data => data)
+      .then(data => data.trips)
     return fetchAllTrips; 
   },
 
   getAllDestinations() {
     const fetchAllDestinations = fetch('http://localhost:3001/api/v1/destinations')
       .then(response => response.json())
-      .then(data => data)
+      .then(data => data.destinations)
     return fetchAllDestinations
   },
 
   getData() {
     return Promise.all([this.getAllTravelers(), this.getAllTrips(), this.getAllDestinations()])
       .then(data => {
-        console.log(data)
-        return data;
+        let allData = [];
+        allData.allTravelers = allData.push(data[0]);
+        allData.allTrips = allData.push(data[1]);
+        allData.allDestinations = allData.push(data[2]);
+        return allData;
       })
   },
 }
