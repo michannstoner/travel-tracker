4733cb97660daf1d1b4009ba9d89bc156b531363
Your Name <stoner.michann@gmail.com>
2021-04-24 12:23:57 -0600

    Fix method that wasn't working on traveler

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,13 +1,50 @@
 import TripRepo from './Trip-Repo';
 
 class Traveler {
-  constructor(allTravelersData) {
+  constructor(allTravelersData, allTripsData, destinationData) {
     this.id = allTravelersData.id;
     this.name = allTravelersData.name;
     this.travelerType = allTravelersData.travelerType;
+    this.tripData = this.getAllUserTrips(allTripsData, destinationData);
   }
+
+  getAllUserTrips(allTripsData, destinationData) {
+    let allTrips = [];
+    const userTrips = allTripsData.filter(trip => {
+      if (trip.userID === this.id) {
+        const newTrip = new TripRepo(trip, destinationData);
+        allTrips.push(newTrip);
+      }
+    });
+    return allTrips;
+  }
+
+  calculateSpentThisYear() {
+  console.log(this.tripData)
+    let yearlyTripCost = this.tripData.filter(trip => {
+      let travelDates = trip.date;
+      // console.log(travelDates)
+      if (new Date(travelDates).getFullYear() === 2020) {
+        // console.log(trip)
+      }
+    })
+  
+  }
+
+  getDate(date) {
+    // const year = date.getFullYear();
+    // const month = date.getFullMonth();
+    // const day = today.getDate();
+    const today = new Date();
+    const justDay = today.getDate();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const fullDate = `${year}/${month}/${justDay}`
+    console.log(fullDate);
+
+  }
+
+
 }
-
-
 
 export default Traveler;
