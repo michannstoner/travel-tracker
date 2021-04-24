d22558985580536cf14f9d9b80a37de9fc2744a0
Your Name <stoner.michann@gmail.com>
2021-04-24 12:24:07 -0600

    Update test to reflect method changes

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -21,11 +21,51 @@ describe('Traveler', () => {
     expect(traveler.id).to.equal(1);
     expect(traveler.name).to.equal('Ham Leadbeater');
     expect(traveler.travelerType).to.equal('relaxer');
-  })
+  });
 
+  it('should store a travelers trips', () => {
+    expect(traveler.tripData).to.deep.equal([
+      {
+        id: 3,
+        userID: 1,
+        destinationID: 3,
+        travelers: 4,
+        date: '2020/05/22',
+        duration: 17,
+        status: 'pending',
+        suggestedActivities: [],
+        destination: {
+          id: 3,
+          destination: 'Sydney, Austrailia',
+          estimatedLodgingCostPerDay: 130,
+          estimatedFlightCostPerPerson: 950,
+          image: 'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+          alt: 'opera house and city buildings on the water with boats'
+        }
+      },
+       {
+        id: 4,
+        userID: 1,
+        destinationID: 4,
+        travelers: 2,
+        date: '2020/02/25',
+        duration: 10,
+        status: 'approved',
+        suggestedActivities: [],
+        destination: {
+          id: 4,
+          destination: 'Cartagena, Colombia',
+          estimatedLodgingCostPerDay: 65,
+          estimatedFlightCostPerPerson: 350,
+          image: 'https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+          alt: 'boats at a dock during the day time'
+        }
+      }
+    ]);
+  });
 
-
-
-
-
+  it('should calculate a travelers amount spent for the year', () => {
+    const travelerCost = traveler.calculateSpentThisYear()
+    expect(travelerCost).to.equal(0);
+  });
 })
